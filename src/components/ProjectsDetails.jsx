50ba7226ccb763204c3e8325e8045722f05f311f
AnaG <you@example.commkanag40@gmail.com>
2026-08-02 15:36:20 +0530

    added pop up animation

--- src/components/ProjectsDetails.jsx
+++ src/components/ProjectsDetails.jsx
@@ -1,4 +1,4 @@
-import {motion} from "motion/react";
+import { motion } from "motion/react";
 
 const ProjectsDetails = ({
   title,
@@ -11,8 +11,13 @@ const ProjectsDetails = ({
 }) => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center w-full h-full overflow-hidden backdrop-blur-sm">
-      <div className="relatuve max-w-2xl border shadow-sm rounded-2xl bng-gradient-to-l from-midnight to-navy border-white/10">
-        <button onClick={closeModal} className="absolute top-4 right-4 p-2 rounded-full bg-white/10 hover:bg-white/20 focus:outline-none focus:ring-2 focus:ring-white">
+      <motion.div className="relatuve max-w-2xl border shadow-sm rounded-2xl bng-gradient-to-l from-midnight to-navy border-white/10"
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}>
+        <button
+          onClick={closeModal}
+          className="absolute top-4 right-4 p-2 rounded-full bg-white/10 hover:bg-white/20 focus:outline-none focus:ring-2 focus:ring-white"
+        >
           <img src="assets/close.svg" alt="close" className="w-6 h-6" />
         </button>
         <img src={image} alt={title} className="w-full h-full rounded-t-2xl" />
@@ -20,21 +25,35 @@ const ProjectsDetails = ({
           <h5 className="text-2xl font-bold mb-2 text-white">{title}</h5>
           <p className="mb-3 font-normal text-neutral-400">{description}</p>
           {subDescription?.map((subDesc, index) => (
-            <p key={index} className="mb-3 font-normal text-neutral-400">{subDesc}</p>
+            <p key={index} className="mb-3 font-normal text-neutral-400">
+              {subDesc}
+            </p>
           ))}
           <div className="flex items-center justify-between mt-4">
             <div className="flex gap-3">
-                {tags.map((tag) => (
-                    <img src={tag.path} key={tag.id}  alt={tag.name} className ="rounded-lg size-10 hover-animation" />
-                ))}
-                <a href={href} className = "inline-flex items-center gap-1font-meddium hover-animation cursor-pointer">
-                    View Projects 
-                    <img src="assets/arrow-up.svg" alt="arrow-up" className="size-4" />
-                </a>
+              {tags.map((tag) => (
+                <img
+                  src={tag.path}
+                  key={tag.id}
+                  alt={tag.name}
+                  className="rounded-lg size-10 hover-animation"
+                />
+              ))}
+              <a
+                href={href}
+                className="inline-flex items-center gap-1font-meddium hover-animation cursor-pointer"
+              >
+                View Projects
+                <img
+                  src="assets/arrow-up.svg"
+                  alt="arrow-up"
+                  className="size-4"
+                />
+              </a>
             </div>
           </div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
